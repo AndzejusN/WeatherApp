63461814139c7b5cb7d5059735d364db71e7bcb5
Andrew <90209255+AndzejusN@users.noreply.github.com>
2021-11-21 22:05:41 +0200

    new file:   .gitignore 	new file:   .idea/WeatherApp.iml 	new file:   .idea/httpRequests/2021-11-12T021145.200.json 	new file:   .idea/httpRequests/2021-11-12T021237.200.json 	new file:   .idea/httpRequests/2021-11-12T021252.200.json 	new file:   .idea/httpRequests/2021-11-12T021303.200.json 	new file:   .idea/httpRequests/2021-11-18T172506.200.json 	new file:   .idea/httpRequests/2021-11-18T172549.200.json 	new file:   .idea/httpRequests/2021-11-18T172710.200.json 	new file:   .idea/httpRequests/http-client.cookies 	new file:   .idea/httpRequests/http-requests-log.http 	new file:   .idea/jsLibraryMappings.xml 	new file:   .idea/modules.xml 	new file:   .idea/vcs.xml 	new file:   .idea/workspace.xml 	new file:   app.js 	new file:   bin/www 	new file:   nodemon.json 	new file:   package-lock.json 	new file:   package.json 	new file:   public/img/bg-2-transp.png 	new file:   public/img/bg-3-transp.png 	new file:   public/img/lt_transparent.png 	new file:   public/img/sky.jpg 	new file:   public/index.html 	new file:   resources/js/app.js 	new file:   resources/sass/app.scss 	new file:   routes/index.js 	new file:   routes/weather.js 	new file:   webpack.mix.js

---
+++ routes/weather.js
@@ -0,0 +1,40 @@
+let express = require('express');
+let router = express.Router();
+let https = require('https');
+
+
+router.get('/places', function(req, res, next)
+{
+	https.get('https://api.meteo.lt/v1/places', response =>{
+		let result = '';
+
+		response.on('data', function(data){
+			result += data.toString();
+		});
+		response.on('end', function(data){
+			res.send(result) ;
+		});
+	});
+});
+
+
+router.get(`/long-term/:place`, function(req, res, next)
+{
+	https.get(`https://api.meteo.lt/v1/places/${req.params['place']}/forecasts/long-term`, response =>{
+		let result = '';
+
+		response.on('data', function(data){
+			result += data.toString();
+		});
+		response.on('end', function(data){
+				res.send(JSON.parse(result));
+		});
+	});
+});
+
+
+router.get('*', (req, res) =>{
+	res.send("This page not found, please check address!");
+})
+
+module.exports = router;
